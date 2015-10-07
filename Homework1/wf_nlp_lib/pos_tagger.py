97e94e892d6478a577134d6fd43b690fd64e8e8f
William Falcon <waf2107@columbia.edu>
2015-10-07 19:01:01 -0400

    working on trigrams

--- Homework1/wf_nlp_lib/pos_tagger.py
+++ Homework1/wf_nlp_lib/pos_tagger.py
@@ -34,17 +34,15 @@ def tag(words_to_tag, possible_tags, known_words, trigram_probs, emission_probs,
 
 def viverti_path(word_list_observations, possible_tags_states, known_words, tags_transition_matrix, emissions_matrix, starting_probabilities):
     path = []
-    vit = viterbi_matrix(word_list_observations, possible_tags_states, emissions_matrix)
+    vit = viterbi_matrix(word_list_observations, possible_tags_states, emissions_matrix, starting_probabilities)
 
-    # Where do I carry the info from the previous pass?
-    prior_state = starting_probabilities
     for i in range(3, len(word_list_observations)):
+        prior_states = vit[i-3:i-1]
         prior_observation_words, current_obs_word = grams_from_list_at_index(i, word_list_observations)
         filtered_word = replace_rare_word_if_needed(known_words, current_obs_word)
-        next_state = calculate_new_probabilities_for_state(prior_state, vit, filtered_word, i-1)
+        next_state = calculate_new_probabilities_for_state(vit, filtered_word, i-1)
 
         predicted_tag = arg_max_from_state(next_state)
-        prior_state = predicted_tag
         tg = ''
         for k, v in predicted_tag.items():
             tg = k
@@ -69,18 +67,25 @@ def get_starting_probabilities(possible_tags_states, tags_transition_matrix):
     return starting_probs
 
 
-def viterbi_matrix(word_list_observations, possible_tags_states, emissions_matrix ):
+def viterbi_matrix(word_list_observations, possible_tags_states, emissions_matrix, starting_probabilities):
     matrix = []
 
     for word in word_list_observations:
         tags = deepcopy(possible_tags_states)
         possible_hidden_states_for_word = transitions_for_prior((word, ), emissions_matrix)
+
+        # handle unseen words (set their probs to -1000)
         if possible_hidden_states_for_word == -1000:
             word = '_RARE_'
             possible_hidden_states_for_word = transitions_for_prior((word, ), emissions_matrix)
 
-        for k, v in possible_hidden_states_for_word.items():
+        # Choose probabilities from starting state or regular word
+        tag_source = starting_probabilities if word == '*' else possible_hidden_states_for_word
+
+        # create the lists
+        for k, v in tag_source.items():
             tags[k] = v
+
         matrix.append({word: tags})
 
     return matrix
@@ -98,25 +103,45 @@ def arg_max_from_state(new_states):
     return {tg: max_val}
 
 
-def calculate_new_probabilities_for_state(prior_state, vit_matrix, word, index):
+def calculate_new_probabilities_for_state(vit_matrix, word, index):
 
     next_state = vit_matrix[index][word]
 
-    # If we came from a non-starting state, we'll only get a max. use that instead
-    if len(prior_state) == 1:
-        for k, v in next_state.items():
-            if k in prior_state:
-                next_state[k] = v * prior_state[k]
-        return  next_state
+    # calc max for prior states
+    prior_states = vit_matrix[index-2:index]
+    prior_maxes = []
+    for state in prior_states:
+        for k, v in state.items():
+            prior_maxes.append(v)
+    max_tag, max_value = calculate_max_path_from_prior_passes_permutations(prior_maxes[1], prior_maxes[0])
 
-    # First time around (from starting state)
-    for k, v in prior_state.items():
-        val = next_state[k] * v
+    # calculate new state
+    for k, v in next_state.items():
+        val = v * max_value
         if val == -0.0:
             val = 0
         next_state[k] = val
 
     return next_state
+
+def calculate_max_path_from_prior_passes_permutations(past1_state, past2_state):
+    """
+    past1_state = prior state
+    past2_state = 2 states ago
+    :param past1_state:
+    :param past2_state:
+    :return:
+    """
+    max_tag = ''
+    max_tag_value = float('inf')
+    for k1, v1 in past1_state.items():
+        for k2, v2 in past2_state.items():
+            product = v2 * v1
+            if product < max_tag_value and product != 0:
+                max_tag_value = product
+                max_tag = k1
+
+    return max_tag, max_tag_value
 
 
 def transition_probabilities_for_ngram_list(ngrams):
